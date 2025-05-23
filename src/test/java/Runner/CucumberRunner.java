03208c66ca250d91deb203a5d51e55c051ce675f
Raavvi861 <rsravvii@gmail.com>
2025-05-23 09:19:07 +0530

    Update CucumberRunner.java

--- src/test/java/Runner/CucumberRunner.java
+++ src/test/java/Runner/CucumberRunner.java
@@ -1,6 +1,7 @@
 package Runner;
-import io.cucumber.testng.CucumberOptions;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features",glue = {"Steps","Hooks"},
 plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},tags = "")
